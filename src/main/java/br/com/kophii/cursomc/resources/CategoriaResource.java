813db092eaa013b914fa156e50bbaab732918015
Wellerson Costa <wellerson@gmail.com>
2026-04-14 21:17:36 -0300

    Testando a primeira Classe de domínio

--- src/main/java/br/com/kophii/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/kophii/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,13 @@
 package br.com.kophii.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.kophii.cursomc.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
@@ -10,8 +16,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CategoriaResource {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando!";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
